3a158233a062d73a64d02b726e44d875cd1dd5f8
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2026-02-27 16:41:23 -0500

    First commit.

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+# Write a program to find out how often a streak of six heads or a streak of six tails comes up in a randomly generated list of 100 heads and tails. Your program should break up the experiment into two parts: the first part generates a list of 100 randomly selected 'H' and 'T' values, and the second part checks if there is a streak in it. Put all of this code in a loop that repeats the experiment 10,000 times so that you can find out what percentage of the coin flips contains a streak of six heads or six tails in a row. As a hint, the function call random.randint(0, 1) will return a 0 value 50 percent of the time and a 1 value the other 50 percent of the time.
+
+import random
+
+for i in range(100):
+    # generate h (0) or t (1)
+    h_or_t = random.randint(0, 1)
+    if h_or_t == 0:
+        print(f'heads')
+    else: print(f'tails')
