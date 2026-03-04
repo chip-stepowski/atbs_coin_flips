2459a7fc3e2ef309974fba71eecd69d6b26abf9f
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2026-03-04 16:16:38 -0500

    Completed finding six matches within set of 100 elements.

--- main.py
+++ main.py
@@ -4,7 +4,14 @@
 
 import random
 
+# the list holding 100 T or H
 final_list = []
+# increment the counter for the current item, and reset it if the next item does not match
+six_counter = 0
+# temp list
+temp_list = []
+# total matches of six
+match_counter = 0
 
 for i in range(100):
     # generate h (0) or t (1)
@@ -14,5 +21,29 @@ for i in range(100):
     else:
         final_list.append('T')
 
-# save to a list
-print(f'final list: {final_list}')
+for i in range(len(final_list) - 1):
+    current_value = final_list[i]
+    next_value = final_list[i + 1]
+    # print the temp list
+    print(f'temp list: {temp_list}')
+    # check if there are six matches of either T or H
+    if six_counter == 6:
+        print('WE GOT 6 MATCHES! INCREASE MATCH COUNTER BY 1')
+        print(f'match counter was: {match_counter}')
+        # increment the match counter to keep track
+        match_counter += 1
+        print(f'match counter is now: {match_counter}')
+        # wipe the list once we have six matches and start again
+        temp_list = []
+    if current_value == next_value:
+        print(f'WE FOUND A MATCH: current value: {current_value}, next value: {next_value}')
+        temp_list.append(current_value)
+        six_counter += 1
+    else:
+        # if the values do not match, wipe the list and reset the six counter
+        print(f'current value: {current_value} does not match next value: {next_value}')
+        temp_list = []
+        six_counter = 0
+
+# total number six matches
+print(f'total matches: {match_counter}')
