dc08d308e1251348da8a6e19b0d72f5570b4762a
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2026-03-05 11:16:53 -0500

    Added counter for 10,000 sets

--- main.py
+++ main.py
@@ -12,38 +12,51 @@ six_counter = 0
 temp_list = []
 # total matches of six
 match_counter = 0
+# 10k counter
+ten_thousand_counter = 1
 
-for i in range(100):
-    # generate h (0) or t (1)
-    h_or_t = random.randint(0, 1)
-    if h_or_t == 0:
-        final_list.append('H')
-    else:
-        final_list.append('T')
+# perform this 10k times
+while ten_thousand_counter <= 10:
+    print(f'Starting run: {ten_thousand_counter}')
+    print(f'===================================')
 
-for i in range(len(final_list) - 1):
-    current_value = final_list[i]
-    next_value = final_list[i + 1]
-    # print the temp list
-    print(f'temp list: {temp_list}')
-    # check if there are six matches of either T or H
-    if six_counter == 6:
-        print('WE GOT 6 MATCHES! INCREASE MATCH COUNTER BY 1')
-        print(f'match counter was: {match_counter}')
-        # increment the match counter to keep track
-        match_counter += 1
-        print(f'match counter is now: {match_counter}')
-        # wipe the list once we have six matches and start again
-        temp_list = []
-    if current_value == next_value:
-        print(f'WE FOUND A MATCH: current value: {current_value}, next value: {next_value}')
-        temp_list.append(current_value)
-        six_counter += 1
-    else:
-        # if the values do not match, wipe the list and reset the six counter
-        print(f'current value: {current_value} does not match next value: {next_value}')
-        temp_list = []
-        six_counter = 0
+    for i in range(100):
+        # generate h (0) or t (1)
+        h_or_t = random.randint(0, 1)
+        if h_or_t == 0:
+            final_list.append('H')
+        else:
+            final_list.append('T')
+
+    for i in range(len(final_list) - 1):
+        current_value = final_list[i]
+        next_value = final_list[i + 1]
+        # print the temp list
+        print(f'temp list: {temp_list}')
+        # check if there are six matches of either T or H
+        if six_counter == 6:
+            print('WE GOT 6 MATCHES! INCREASE MATCH COUNTER BY 1')
+            print(f'match counter was: {match_counter}')
+            # increment the match counter to keep track
+            match_counter += 1
+            print(f'match counter is now: {match_counter}')
+            # wipe the list once we have six matches and start again
+            temp_list = []
+        if current_value == next_value:
+            print(f'WE FOUND A MATCH: current value: {current_value}, next value: {next_value}')
+            temp_list.append(current_value)
+            six_counter += 1
+        else:
+            # if the values do not match, wipe the list and reset the six counter
+            print(f'Current value: {current_value} does not match next value: {next_value}')
+            temp_list = []
+            six_counter = 0
+    # message that the first set has finished
+    print(f'Set {ten_thousand_counter} has finished')
+    # reset the list for the next set
+    temp_list = []
+    # increment ten thousand counter
+    ten_thousand_counter += 1
 
 # total number six matches
-print(f'total matches: {match_counter}')
+print(f'total matches from 10 runs: {match_counter}')
